44ae90a739250c12ccf79f7a1972b58731fcd6c1
DWL22 <candra.icy@gmail.com>
2025-03-05 10:42:00 +0800

    event to object Kelar

--- 5.dom/eventtoobject/app.js
+++ 5.dom/eventtoobject/app.js
@@ -1,1 +1,29 @@
-document.query;
+document.querySelector("button").addEventListener("click", (e) => {
+  console.log(e);
+});
+
+const input = document.querySelector("input");
+input.addEventListener("keydown", (e) => {
+  switch (e.code) {
+    case "ArrowUp":
+      console.log("tombol arah atas");
+      break;
+
+    case "ArrowDown":
+      console.log("tombol arah bawah");
+      break;
+
+    case "ArrowLeft":
+      console.log("tombol arah Kiri");
+      break;
+
+    case "ArrowRight":
+      console.log("tombol arah Kanan");
+      break;
+    default:
+      console.log("diabaikan");
+  }
+});
+// input.addEventListener("keyup", () => {
+//   console.log("tombol di lepas");
+// });
